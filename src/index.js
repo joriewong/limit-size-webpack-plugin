8de60df4bcb43c70bdc88ba69354170b6d3ed57e
joriewong <stillba@icloud.com>
2021-04-10 10:51:04 +0800

    fix: import

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-class LimitSizePlugin {
+import chalk from "chalk";
+import { lint } from "./lint";
+import { logResult } from "./log";
+
+export class LimitSizePlugin {
   constructor(files = []) {
     this.files = files;
   }
 
   apply(compiler) {
-    compiler.hooks.done.tap(
-      "LimitSizePlugin",
-      async () => {
-        /**
-         * limit-size
-         */
-        // 1. lint size limit
-        const result = await lint(this.files);
-        // 2. log
-        logResult(result);
-        // 3. help doc
-        const success = result.every((r) => r.passed);
-        if (!success) {
-          console.log(
-            chalk.bold.yellow(
-              'Try to reduce size or increase limit in "limit-size"'
-            ),
-            "\n"
-          );
-        }
+    compiler.hooks.done.tap("LimitSizePlugin", async () => {
+      /**
+       * limit-size
+       */
+      // 1. lint size limit
+      const result = await lint(this.files);
+      // 2. log
+      logResult(result);
+      // 3. help doc
+      const success = result.every((r) => r.passed);
+      if (!success) {
+        console.log(
+          chalk.bold.yellow(
+            'Try to reduce size or increase limit in "limit-size"'
+          ),
+          "\n"
+        );
       }
-    );
+    });
   }
 }
-
-export default LimitSizePlugin
